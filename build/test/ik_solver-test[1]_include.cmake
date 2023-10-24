0b3be37fc58249b40b14dfa2ffed5aecfcef0b01
Tej Kiran <itej89@gmail.com>
2023-10-24 19:10:18 -0400

    1.5 Added cpp-check and cpp-lint; \n 1.3.3.2 Updated robot.hpp; \n 1.4.1.2 include directory location correction
    Solved the cpp-lint, cpp-check errors. \n Added Default constructor to robot class in robot.cpp. Moved include directory into the ik_solver library directory. Removed build folder folder from git commit.

--- build/test/ik_solver-test[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/tej/Documents/Maryland/Semester4/808x/HW/OCt31/Manipulator_Motion_Planner_IK_Solver/build/test/ik_solver-test[1]_tests.cmake")
-  include("/home/tej/Documents/Maryland/Semester4/808x/HW/OCt31/Manipulator_Motion_Planner_IK_Solver/build/test/ik_solver-test[1]_tests.cmake")
-else()
-  add_test(ik_solver-test_NOT_BUILT ik_solver-test_NOT_BUILT)
-endif()
