2db6b274cc148ae90117d24e284836350d58c1fd
syed <syedabrar56345@gmail.com>
2023-11-01 00:23:13 -0400

    added cppcheck & cpplint

--- libs/ik_solver/include/robot.hpp
+++ libs/ik_solver/include/robot.hpp
@@ -1,4 +1,34 @@
 #pragma once
+/**
+ * @file robot.cpp
+ *
+ * @brief Represents a robot with Denavit-Hartenberg parameters.
+ *
+ * This class extends the DHParameters class to create a robot with additional
+ * properties and methods.
+ *
+ * @author Krishna Rajesh Hundekari
+
+ * Apache License Version 2.0, January 2004
+
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+* 
+*   http://www.apache.org/licenses/LICENSE-2.0
+* 
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+ */
+
 
 #include "DHParameters.hpp"
 #include <iostream>
@@ -10,16 +40,7 @@
 #include <algorithm>
 #include <Eigen/Dense>
 
-/**
- * @file robot.hpp
- *
- * @brief Represents a robot with Denavit-Hartenberg parameters.
- *
- * This class extends the DHParameters class to create a robot with additional
- * properties and methods.
- *
- * @author Krishna Rajesh Hundekari
- */
+
 class Robot {
 public:
 
